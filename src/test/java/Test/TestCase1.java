093a21ff7aadf16ea9465807f43830bcd6d1a061
Pathange Gaurav <126910570+Gaurav-GitHub-Repository@users.noreply.github.com>
2024-12-31 13:12:23 +0530

    Update TestCase1.java

--- src/test/java/Test/TestCase1.java
+++ src/test/java/Test/TestCase1.java
@@ -21,50 +21,8 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 
-@Listeners(utility.class)
-public class TestCase1 extends browserconfig{
-	protected static WebDriver driver;
 
-	final String URL = "https://testautomationpractice.blogspot.com/";
-	
-	@Parameters({"browser"})
-	@Test(priority=1)
-	public void setup(String browser) throws MalformedURLException
-	{
-		if(browser.equalsIgnoreCase("GoogleChrome"))
-		{
-			ChromeOptions option = new ChromeOptions();
-			System.setProperty("webdriver.chrome.driver", "C:\\Eclipse-Selenium 13-07-2024\\eclipse-java\\Selenium_Concept\\drivers\\Chrome_V131\\chromedriver.exe");
-			//option.addArguments("--headless");
-			driver = new ChromeDriver(option);
-			driver.manage().window().maximize();
-			driver.get(URL);
-			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		}
-		if(browser.equalsIgnoreCase("MozillaFirefox"))
-		{
-			FirefoxOptions option = new FirefoxOptions();
-			System.setProperty("webdriver.gecko.driver", "C:\\Eclipse-Selenium 13-07-2024\\eclipse-java\\Selenium_Concept\\drivers\\geckodriver.exe");
-			//option.addArguments("--headless");
-			driver = new FirefoxDriver(option);
-			driver.manage().window().maximize();
-			driver.get(URL);
-			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		}
-		if(browser.equalsIgnoreCase("MicrosoftEdge"))
-		{
-			System.setProperty("webdriver.edge.driver", "C:\\Eclipse-Selenium 13-07-2024\\eclipse-java\\Selenium_Concept\\drivers\\msedgedriver.exe");
-			//option.addArguments("--setHeadless");
-			driver = new EdgeDriver();
-			driver.manage().window().maximize();
-			driver.get(URL);
-			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		}
-		else
-		{
-			System.out.println("Browser is not launched");
-		}
-	}
+public class TestCase1 extends browserconfig{
 
 	@Description("Verify GUI Elements text is present in dashboard")
 	@Epic("TC_001")
